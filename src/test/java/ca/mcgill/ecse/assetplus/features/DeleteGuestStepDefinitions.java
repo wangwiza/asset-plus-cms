a0920a9f6982e429478a8bb651a530ad416f0ffd
wangwiza <76924804+wangwiza@users.noreply.github.com>
2023-10-08 19:47:24 -0400

    (chore): added author to step definition

--- src/test/java/ca/mcgill/ecse/assetplus/features/DeleteGuestStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/assetplus/features/DeleteGuestStepDefinitions.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class DeleteGuestStepDefinitions {
+  @author("William Wang")
   @Given("the following guests exist in the system \\(p8)")
   public void the_following_guests_exist_in_the_system_p8(
       io.cucumber.datatable.DataTable dataTable) {
@@ -18,6 +19,7 @@ public class DeleteGuestStepDefinitions {
     throw new io.cucumber.java.PendingException();
   }
 
+  @author("Krasimir Kirov")
   @Given("the following manager exists in the system \\(p8)")
   public void the_following_manager_exists_in_the_system_p8(
       io.cucumber.datatable.DataTable dataTable) {
@@ -31,24 +33,28 @@ public class DeleteGuestStepDefinitions {
     throw new io.cucumber.java.PendingException();
   }
 
+  @author("Michael Rafferty")
   @When("the guest attempts to delete their own account linked to the {string} \\(p8)")
   public void the_guest_attempts_to_delete_their_own_account_linked_to_the_p8(String string) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
+  @author("Vlad Arama")
   @Then("the guest account linked to {string} shall not exist in the system \\(p8)")
   public void the_guest_account_linked_to_shall_not_exist_in_the_system_p8(String string) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
+  @author("Li Yang Lei")
   @Then("the manager account linked to {string} shall exist in the system \\(p8)")
   public void the_manager_account_linked_to_shall_exist_in_the_system_p8(String string) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
+  @author("Tim Pham")
   @Then("the number of guests in the system shall be {string} \\(p8)")
   public void the_number_of_guests_in_the_system_shall_be_p8(String string) {
     // Write code here that turns the phrase above into concrete actions
