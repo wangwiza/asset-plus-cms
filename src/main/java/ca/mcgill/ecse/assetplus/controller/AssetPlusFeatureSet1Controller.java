1c239753435d7d99f6b72b50feaee742e039a40f
LeiLiYang <liyanglei23@gmail.com>
2023-10-14 15:44:34 -0400

    added more complete features - controller1

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet1Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet1Controller.java
@@ -15,6 +15,18 @@ public class AssetPlusFeatureSet1Controller {
   private AssetPlusFeatureSet1Controller() {};
   public static String updateManager(String password) throws Exception {
     try {
+      if (password.length()<4) {
+        throw new Exception("Password must be at least four characters long");
+      }
+      if (password==password.toLowerCase()) {
+        throw new Exception("Password must contain one upper-case character");
+      }
+      if (password==password.toUpperCase()) {
+        throw new Exception("Password must contain one lower-case character");
+      }
+      if (!(password.contains("!")|| password.contains("#") || password.contains("$"))) {
+        throw new Exception("Password must contain one character out of !#$");
+      }
       ap.getManager().setPassword(password);
       return "";
     } catch (Exception e) {
@@ -36,23 +48,37 @@ public class AssetPlusFeatureSet1Controller {
       }
       return "";
     } catch (Exception e) {
-      throw new Exception("An error has occured. Please try again.");
+      throw new Exception("An error has occured. Please try again: " + e);
     }
   }
 
   public static String updateEmployeeOrGuest(String email, String newPassword, String newName, String newPhoneNumber) throws Exception {
     try {
       List<Employee> employeeList = ap.getEmployees();
+      List<Guest> guestList = ap.getGuests();
+      boolean isFound = false;
       for (int i=0; i<employeeList.size(); i++) {
         if (employeeList.get(i).getEmail()==email){
           employeeList.get(i).setPassword(newPassword);
           employeeList.get(i).setPhoneNumber(newPhoneNumber);
           employeeList.get(i).setName(newName);
+          isFound = true;
           break;
         }
       }
+    if (!isFound) {
+      for (int i=0; i<guestList.size(); i++) {
+        if (guestList.get(i).getEmail()==email){
+          guestList.get(i).setPassword(newPassword);
+          guestList.get(i).setPhoneNumber(newPhoneNumber);
+          guestList.get(i).setName(newName);
+          isFound = true;
+          break;
+        }
+      }
+    }
     } catch (Exception e) {
-      throw new Exception("An error has occured. Please try again.");
+      throw new Exception("An error has occured. Please try again: " + e);
     }
     return "";
   }
