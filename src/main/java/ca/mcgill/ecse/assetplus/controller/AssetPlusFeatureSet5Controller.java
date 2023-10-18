837871deaddf12619482f1dbf988f75158687b7e
wangwiza <76924804+wangwiza@users.noreply.github.com>
2023-10-17 22:29:56 -0400

    (feat): ControllerFS5 complete with javadoc

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -7,13 +7,20 @@ import ca.mcgill.ecse.assetplus.model.TicketImage;
 
 public class AssetPlusFeatureSet5Controller {
 
-  // access to root application object
-  private static AssetPlus ap = AssetPlusApplication.getAssetPlus();
+  // // access to root application object
+  // private static AssetPlus ap = AssetPlusApplication.getAssetPlus();
 
   // controller feature set shouldn't be instantiated
   private AssetPlusFeatureSet5Controller() {};
 
-
+  /**
+   * Adds a ticket image to a specific ticket
+   * 
+   * @author William Wang
+   * @param imageURL
+   * @param ticketID
+   * @return Error message from either invalid input or RuntimeException
+   */
   public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
     // input validation
     var error = "";
@@ -42,18 +49,33 @@ public class AssetPlusFeatureSet5Controller {
     try {
       ticket.addTicketImage(imageURL);
     } catch (RuntimeException e) {
-      // error = e.getMessage();
-      // if (error.startsWith("known error prefix")) {  // TODO: figure out what throws error
-      //   error = "alternative text";
-      // }
+      error = e.getMessage();
+      if (error.startsWith("Unable to create ticketImage due to ticket.")) {
+        error = "There must be at least one ticket for a ticket image to exist.";
+      }
       return e.getMessage();
     }
     return error;
   }
 
+  /**
+   * Deletes an image from a maintenance ticket if the specified ticket exists and its ticket images contains the image to be deleted.
+   * 
+   * @author Willian Wang
+   * @param imageURL
+   * @param ticketID
+   */
   public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
+    if (ticket != null) {
+      List<TicketImage> ticketImages = ticket.getTicketImages();
+      for (TicketImage ticketImage: ticketImages) {
+        if (ticketImage.getImageURL() == imageURL) {
+          ticket.removeTicketImage(ticketImage);
+          break;
+        }
+      }
+    }
   }
 
 }
