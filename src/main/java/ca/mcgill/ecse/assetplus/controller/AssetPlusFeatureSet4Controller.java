c00676ee7eb132c9bd4f8cf450e9edab22db610b
timmyhoa <timmyhoa34@gmail.com>
2023-10-17 22:44:59 -0400

    Implementation of set4 controller completed

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet4Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet4Controller.java
@@ -3,17 +3,18 @@ package ca.mcgill.ecse.assetplus.controller;
 import java.sql.Date;
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
-import ca.mcgill.ecse.assetplus.model.Guest;
 import ca.mcgill.ecse.assetplus.model.MaintenanceTicket;
+import ca.mcgill.ecse.assetplus.model.SpecificAsset;
+import ca.mcgill.ecse.assetplus.model.User;
 
 public class AssetPlusFeatureSet4Controller {
   private static AssetPlus ap = AssetPlusApplication.getAssetPlus();
 
   // assetNumber -1 means that no asset is specified
   public static String addMaintenanceTicket(int id, Date raisedOnDate, String description, String email, int assetNumber) {
-    Guest g = Util.getGuest(email);
+    User g = Util.getUser(email);
     if (g.equals(null)) {
-      return "User does not exists";
+      return "The ticket raiser does not exist";
     }
     MaintenanceTicket t = ap.addMaintenanceTicket(id, raisedOnDate, description, g);
     t.setAsset(ap.getSpecificAsset(assetNumber));
@@ -23,13 +24,45 @@ public class AssetPlusFeatureSet4Controller {
   // newAssetNumber -1 means that no asset is specified
   public static String updateMaintenanceTicket(int id, Date newRaisedOnDate, String newDescription,
       String newEmail, int newAssetNumber) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+      MaintenanceTicket current = ap.getMaintenanceTicket(id);
+      if (current == null) {
+        return "";
+      }
+
+      current.setRaisedOnDate(newRaisedOnDate);
+
+      if (newDescription.isBlank()) {
+        return "Ticket description cannot be empty";
+      }
+
+      current.setDescription(newDescription);
+
+      User u = Util.getUser(newEmail);
+      if (u == null) {
+        return "Email does not match any guests";
+      }
+
+      SpecificAsset a;
+      if (newAssetNumber == 0) {
+        a = null;
+      } else {
+        a = ap.getSpecificAsset(newAssetNumber);
+        if (a == null) {
+          return "The asset does not exist";
+        }
+      }
+
+      current.setTicketRaiser(u);
+      current.setAsset(a);
+      return "";
   }
 
   public static void deleteMaintenanceTicket(int id) {
     // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    MaintenanceTicket t = ap.getMaintenanceTicket(id);
+    if (t == null) {
+      return;
+    }
+    t.delete();
   }
-
 }
--- src/main/java/ca/mcgill/ecse/assetplus/controller/Util.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/Util.java
@@ -1,18 +1,23 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import java.util.Collections;
 import java.util.List;
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
-import ca.mcgill.ecse.assetplus.model.Guest;
+import ca.mcgill.ecse.assetplus.model.User;
 
 public class Util {
   private static AssetPlus ap = AssetPlusApplication.getAssetPlus();
 
-  public static Guest getGuest(String email) {
-    List<Guest> guests = ap.getGuests();
-    for (Guest guest: guests) {
-      if (guest.getEmail().equals(email)) {
-        return guest;
+  public static User getUser(String email) {
+    List<User> allUsers = Collections.emptyList();
+    allUsers.addAll(ap.getGuests());
+    allUsers.addAll(ap.getEmployees());
+    allUsers.add(ap.getManager());
+
+    for (User u: allUsers) {
+      if (u.getEmail().equals(email)) {
+        return u;
       }
     }
     return null;
