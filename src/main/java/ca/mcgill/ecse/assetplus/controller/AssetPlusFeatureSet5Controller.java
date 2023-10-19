774aa30cd9d69e368826c851e422e394c5b5fea6
wangwiza <william.wang4@mail.mcgill.ca>
2023-10-19 11:36:29 -0400

    (fix): FS5 error catching

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -1,14 +1,38 @@
 package ca.mcgill.ecse.assetplus.controller;
+import java.sql.Date;
 import java.util.List;
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
+import ca.mcgill.ecse.assetplus.model.AssetType;
+import ca.mcgill.ecse.assetplus.model.Employee;
 import ca.mcgill.ecse.assetplus.model.MaintenanceTicket;
+import ca.mcgill.ecse.assetplus.model.Manager;
+import ca.mcgill.ecse.assetplus.model.SpecificAsset;
 import ca.mcgill.ecse.assetplus.model.TicketImage;
 
 public class AssetPlusFeatureSet5Controller {
-
-  // // access to root application object
-  // private static AssetPlus ap = AssetPlusApplication.getAssetPlus();
+  public static void main(String[] args) {
+    // testing
+    // Given
+    AssetPlus ap = AssetPlusApplication.getAssetPlus();
+    System.out.println(ap.hasEmployees());
+    ap.addEmployee("jeff@ap.com", "pass1", "Jeff", "(555)555-5555");
+    Employee smith = ap.addEmployee("smith@ap.com", "pass1", "Smith", "(555)555-5555");
+    System.out.println(ap.hasEmployees());
+    Manager manager = new Manager("manager@ap.com", null, "manager", null, ap);
+    AssetType lamp = ap.addAssetType("lamp", 1800);
+    AssetType bed = ap.addAssetType("bed", 5000);
+    SpecificAsset aLamp = ap.addSpecificAsset(1, 9, 23, new Date(2022, 3, 20), lamp);
+    SpecificAsset aBed = ap.addSpecificAsset(2, 10, 35, new Date(2022, 1, 30), bed);
+    MaintenanceTicket ticket1 = ap.addMaintenanceTicket(1, new Date(2023,7,20), "haha funny", manager);
+    MaintenanceTicket ticket2 = ap.addMaintenanceTicket(2, new Date(2023,7,10), "haha funny", smith);
+    ticket1.addTicketImage("https://imageurl.com/i.jpg");
+    ticket1.addTicketImage("http://thisimage.com/1.png");
+    // When
+    AssetPlusFeatureSet5Controller.addImageToMaintenanceTicket("https://imageurl.com/j.jpg", 1);
+    System.out.println(ticket1.getTicketImages());
+    //Then
+  }
 
   // controller feature set shouldn't be instantiated
   private AssetPlusFeatureSet5Controller() {};
@@ -22,22 +46,23 @@ public class AssetPlusFeatureSet5Controller {
    * @return Error message from either invalid input or RuntimeException
    */
   public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
+    
     // input validation
     var error = "";
     if (imageURL == null || imageURL.trim().isEmpty()) {
-      error = "Image URL cannot be empty";
+      error = "Image URL cannot be empty. ";
     }
     if (!(imageURL.startsWith("http://") || imageURL.startsWith("https://"))) {
-      error = "Image URL must start with http:// or https://";
+      error += "Image URL must start with http:// or https://. ";
     }
     MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
     if (ticket == null) {
-      error = "Ticket does not exist";
+      error += "Ticket does not exist. ";
     }
     List<TicketImage> ticketImages = ticket.getTicketImages();
     for (TicketImage e: ticketImages) {
       if (e.getImageURL() == imageURL) {
-        error = "Image already exists for the ticket";
+        error += "Image already exists for the ticket. ";
         break;
       }
     }
@@ -49,13 +74,13 @@ public class AssetPlusFeatureSet5Controller {
     try {
       ticket.addTicketImage(imageURL);
     } catch (RuntimeException e) {
-      error = e.getMessage();
-      if (error.startsWith("Unable to create ticketImage due to ticket.")) {
-        error = "There must be at least one ticket for a ticket image to exist.";
+      String caughtError = e.getMessage();
+      if (caughtError.startsWith("Unable to create ticketImage due to ticket.")) {
+        caughtError = "There must be at least one ticket for a ticket image to exist. ";
       }
-      return e.getMessage();
+      return caughtError;
     }
-    return error;
+    return "";
   }
 
   /**
