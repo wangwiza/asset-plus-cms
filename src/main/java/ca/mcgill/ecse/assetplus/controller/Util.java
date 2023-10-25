f6aeb6be9bb4d0a1c6bb0182922eec5e583c207c
timmyhoa <timmyhoa34@gmail.com>
2023-10-25 16:24:45 -0400

    feature set 4 now working, changes to Util class

--- src/main/java/ca/mcgill/ecse/assetplus/controller/Util.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/Util.java
@@ -1,16 +1,18 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import ca.mcgill.ecse.assetplus.model.AssetPlus;
+import ca.mcgill.ecse.assetplus.model.SpecificAsset;
 import ca.mcgill.ecse.assetplus.model.User;
 
 public class Util {
   private static AssetPlus ap = AssetPlusApplication.getAssetPlus();
 
   public static User getUser(String email) {
-    List<User> allUsers = Collections.emptyList();
+    ArrayList<User> allUsers = new java.util.ArrayList<>(Collections.emptyList());
     allUsers.addAll(ap.getGuests());
     allUsers.addAll(ap.getEmployees());
     allUsers.add(ap.getManager());
@@ -22,4 +24,15 @@ public class Util {
     }
     return null;
   }
+
+  public static SpecificAsset getSpecificAsset(int id) {
+    List<SpecificAsset> allAsset = ap.getSpecificAssets();
+    for (SpecificAsset a: allAsset) {
+      if (a.getAssetNumber() == id) {
+        return a;
+      }
+    }
+    return null;
+  }
+
 }
