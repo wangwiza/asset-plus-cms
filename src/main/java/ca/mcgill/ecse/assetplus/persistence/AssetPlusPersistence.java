60b7c80448e1d8c881253f64337af28663122d20
wangwiza <william.wang4@mail.mcgill.ca>
2023-10-31 08:23:14 -0400

    (feat): persistence package done

---
+++ src/main/java/ca/mcgill/ecse/assetplus/persistence/AssetPlusPersistence.java
@@ -0,0 +1,33 @@
+package ca.mcgill.ecse.assetplus.persistence;
+
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.AssetPlus;
+
+public class AssetPlusPersistence {
+  private static String filename = "data.json";
+  private static JsonSerializer serializer = new JsonSerializer("ca.mcgill.ecse.assetplus");
+
+  public static void setFilename(String filename) {
+    AssetPlusPersistence.filename = filename;
+  }
+
+  public static void save() {
+    save(AssetPlusApplication.getAssetPlus());
+  }
+
+  public static void save(AssetPlus ap) {
+    serializer.serialize(ap, filename);
+  }
+
+  public static AssetPlus load() {
+    var ap = (AssetPlus) serializer.deserialize(filename);
+    // model cannot be loaded - create empty AssetPlus
+    if (ap == null) {
+      ap = new AssetPlus();
+    } else {
+      ap.reinitialize();
+    }
+    return ap;
+  }
+
+}
