76afd6f341f3b3f74fd52c5f71b929e4222e6605
Vlad Arama <vladarama2002@gmail.com>
2023-11-25 16:39:07 -0500

    fix UpdateAssetTypeController

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/controllers/UpdateAssetTypeController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/controllers/UpdateAssetTypeController.java
@@ -2,15 +2,14 @@ package ca.mcgill.ecse.assetplus.javafx.controllers;
 
 import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet2Controller;
 import static ca.mcgill.ecse.assetplus.javafx.controllers.ViewUtils.successful;
-import java.sql.Date;
 import static ca.mcgill.ecse.assetplus.javafx.controllers.ViewUtils.sceneSwitch;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-public class UpdateAssetTypeController {
 
+public class UpdateAssetTypeController {
 
   @FXML
   private TextField updateName;
@@ -28,6 +27,9 @@ public class UpdateAssetTypeController {
   private AnchorPane updateAssetTypeAnchorPane;
 
   @FXML
+  private String oldName;
+
+  @FXML
   void cancelUpdateAssetTypeClicked(ActionEvent event) {
     updateName.setText("");
     updateExpectedLifespan.setText("");
@@ -36,10 +38,10 @@ public class UpdateAssetTypeController {
 
   @FXML
   void updateAssetTypeClicked(ActionEvent event) {
-    String oldName = "lamp";
     String name = updateName.getText();
     Integer expectedLifespan = Integer.parseInt(updateExpectedLifespan.getText());
-    if (successful(AssetPlusFeatureSet2Controller.updateAssetType(oldName, name, expectedLifespan))) {
+    if (successful(
+        AssetPlusFeatureSet2Controller.updateAssetType(this.oldName, name, expectedLifespan))) {
       updateName.setText("");
       updateExpectedLifespan.setText("");
       sceneSwitch(updateAssetTypeAnchorPane, "../pages/AssetTypeView.fxml");
@@ -47,6 +49,7 @@ public class UpdateAssetTypeController {
   }
 
   public void setName(String name) {
+    this.oldName = name;
     updateName.setText(name);
   }
 
