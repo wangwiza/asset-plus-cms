89f4e0668b0670f9cea0fac9aa64a1963b79b10a
LeiLiYang <liyanglei23@gmail.com>
2023-11-27 20:54:03 -0500

    fixed refresh

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/controllers/MainPageController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/controllers/MainPageController.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse.assetplus.javafx.controllers;
 
+import static ca.mcgill.ecse.assetplus.javafx.controllers.ViewUtils.sceneSwitch;
 import ca.mcgill.ecse.assetplus.javafx.AssetPlusFxmlView;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -7,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class MainPageController {
@@ -48,30 +50,36 @@ public class MainPageController {
   @FXML
   public void dashboardUsersSelected(ActionEvent event) {
     usersPane.toFront();
+    sceneSwitch((AnchorPane) usersPane, "../pages/UserView.fxml");
   }
 
   @FXML
   public void dashboardImagesSelected(ActionEvent event) {
     imagesPane.toFront();
+    sceneSwitch((AnchorPane) imagesPane, "../pages/ImagesPage.fxml");
   }
 
   @FXML
   public void dashboardTicketsSelected(ActionEvent event) { 
     ticketsPane.toFront(); 
+    sceneSwitch((AnchorPane) ticketsPane, "../pages/TicketsPage.fxml");
   }
 
   @FXML
   public void dashboardAssetTypesSelected(ActionEvent event) {
     assetTypesPane.toFront();
+    sceneSwitch((AnchorPane) assetTypesPane, "../pages/AssetTypeView.fxml");
   }
 
   @FXML
   public void dashboardAssetsSelected(ActionEvent event) {
     assetsPane.toFront();
+    sceneSwitch((AnchorPane) assetsPane, "../pages/AssetView.fxml");
   }
 
   public void dashboardViewMaintenanceTicketsSelected(ActionEvent event) {
     maintenanceTicketsPane.toFront();
+    sceneSwitch((AnchorPane) maintenanceTicketsPane, "../pages/TicketsPage.fxml");
   }
 
 }
